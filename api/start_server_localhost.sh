319696f3eeeaaa34344b6730a91ad9298fab7f93
Yuki Waka <ywaka@klick.com>
2023-03-14 17:23:50 -0400

    Removed file

--- api/start_server_localhost.sh
+++
@@ -1,6 +0,0 @@
-export DATABASE_USERNAME="ywaka"
-export DATABASE_PASSWORD="goaxjflek8C!"
-export DATABASE_URL="localhost"
-export DATABASE_PORT=5432
-export DATABASE_NAME="chat_gpt_test";
-node api
